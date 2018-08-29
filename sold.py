2057fb47347c72bdca3169e237c8f2ee9a209528
Byonurex <42118607+Byonurex@users.noreply.github.com>
2018-08-30 02:20:41 +0300

    Add files via upload

---
+++ sold.py
@@ -0,0 +1,18 @@
+import sys
+import time
+import os
+from urllib2 import *
+from platform import system
+
+def ping():
+	print
+	print
+	os.system("toilet -f pagga 'Onur Attack       ' | lolcat")
+	time.sleep(1)
+	print
+	
+	os.system("curl https://api.hackertarget.com/nping/?q=" + ip)
+	print(" ")
+	
+ping()
+sys.exit()
