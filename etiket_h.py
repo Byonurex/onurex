fce5d0a35b2b65b1517727dac07b988f0762d3f9
Byonurex <42118607+Byonurex@users.noreply.github.com>
2018-09-09 20:16:25 +0300

    Add files via upload

---
+++ etiket_h.py
@@ -0,0 +1,39 @@
+import urllib.request
+import re
+
+try:
+    #Adresi Aldık
+    onurex = input("SAH> ")
+    print("60 Saniye içinde cevap gelmezse yeniden deneyiniz !")
+    print("\n")
+
+    html = urllib.request.urlopen(onurex)
+    html = html.read()
+    html = html.decode('utf-8')
+    html = str(html)
+
+    #regex ile etiketi belirtelim
+    bulucu = "<title>(.*?)</title>"
+
+    ara = re.search(bulucu, html)
+
+    if ara:
+        etiket = ara.group(0)
+
+        icerik = ara.group(1)
+
+        print("Etiket: " + etiket + "\n")
+        print("İçerik: " + icerik)
+
+    else:
+        print("Etiket bulunamadı Hata !!!")
+
+except:
+        print("""
+        Hata Oluşu 
+        Hata Nedenleri...
+        Sitenin sonuna / koydunmu ?
+        Http / Https Koydunmu ?
+        Yanlışmı yazdın ?
+        Örnek : http://gmods.ml/
+        """)
